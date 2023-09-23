0faccbbed6dd81cbd03be07fa33ab3ac9ccdef5b
Saurabh_Chavhan <145839827+Saurabh-Chavhan-02@users.noreply.github.com>
2023-09-23 20:12:17 +0530

    Add files via upload

---
+++ First.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    //variable declare 4 bytes
+    int a=10, b=20, c=30, d, e=11;
+    cout<<d;
+    char ch = 'a';
+    cout<<'a'<<endl;
+    float f = 2.31;
+    cout<<f<<endl;
+    double d = 3.121;
+    cout<<d<<endl;
+    bool b = 0;
+    cout<<b;
+};
